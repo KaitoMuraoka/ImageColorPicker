d11911f7be7d6a732a1c07b568b6d64aaeeb2661
KaitoMuraoka <kaitoboshi@icloud.com>
2022-02-01 11:14:30 +0900

    Initial Commit

---
+++ ImageColorPicker/ImageColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageColorPickerApp.swift
+//  ImageColorPicker
+//
+//  Created by 村岡海人 on 2022/02/01.
+//
+
+import SwiftUI
+
+@main
+struct ImageColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
