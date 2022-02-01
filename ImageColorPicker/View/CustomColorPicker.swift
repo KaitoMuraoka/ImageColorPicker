bcb9fefd955e2c5726151e3509c9875b20914655
KaitoMuraoka <kaitoboshi@icloud.com>
2022-02-01 15:09:19 +0900

    first commit

---
+++ ImageColorPicker/View/CustomColorPicker.swift
@@ -0,0 +1,20 @@
+//
+//  CustomColorPicker.swift
+//  ImageColorPicker
+//
+//  Created by 村岡海人 on 2022/02/01.
+//
+
+import SwiftUI
+
+struct CustomColorPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomColorPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomColorPicker()
+    }
+}
---
+++ ImageColorPicker/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  ImageColorPicker
+//
+//  Created by 村岡海人 on 2022/02/01.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
